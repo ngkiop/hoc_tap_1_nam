df8537c193feecc3cc432d6db5f6965ddcf4a9ff
Ngk <ngocztoong18@gmail.com>
2025-09-03 16:51:07 +0700

    Hoàn thành ngày 1

---
+++ day1.py
@@ -0,0 +1,63 @@
+# print("Xin chào kỹ sư phần mềm tương lai hẹ hẹ hẹ")
+# name = "Ngk"
+# age = 27
+# height = 1.75
+# is_student = False
+# print(name, age, height, is_student)
+
+# print("Xin chào, tôi là",name,"và tôi",age,"tuổi")
+
+# name = input("Nhập tên của bạn: ")
+# age = input("Nhập tuổi của bạn: ")
+
+# print("Xin chào, tôi là", name, "và tôi", age, "tuổi")
+
+# a = float(input("Nhập số thứ nhất: "))
+# b = float(input("Nhập số thứ hai: "))
+
+# print("Tổng:", a + b)
+# print("Hiệu:", a - b)
+# print("Tích:", a * b)
+# print("Thương:", a / b)
+# print("Chia lấy dư:", a % b)
+# print("Lũy thừa:", a ** b)
+# print("Chia lấy phần nguyên:", a // b)
+# print("Căn bậc hai của a:", a ** 0.5)
+# print("Căn bậc hai của b:", b ** 0.5)
+# print("Giá trị tuyệt đối của a:", abs(a))
+# print("Giá trị tuyệt đối của b:", abs(b))
+# print("Số lớn hơn:", max(a, b))
+# print("Số nhỏ hơn:", min(a, b))
+
+
+# import math
+
+# a = float(input("Nhập bán kính r: "))
+# s = math.pi * a ** 2
+
+# print("Diện tích hình tròn:", s)
+
+
+# a = float(input("Nhập nhiệt độ (°C): "))
+
+# b = a * 9/5 + 32 
+
+# print("Nhiệt độ (°F):", b)
+
+# a = float(input("Nhập số thứ nhất: "))
+
+# b = float(input("Nhập số thứ hai: "))
+
+# c = float(input("Nhập số thứ ba: "))
+
+# d = (a + b + c) / 3
+
+# print("Trung bình cộng:", d)
+
+n = int(input("Nhập số nguyên dương n: "))
+
+if n > 0:
+    if n % 2 == 0:
+        print(n, "là số chẵn")
+    else:
+        print(n, "là số lẻ")                
